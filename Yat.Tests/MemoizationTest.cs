668421758af7b53f8db52fd15ae9d5dfdcc2cef9
Arialdo Martini <arialdomartini@gmail.com>
2014-12-18 23:32:32 +0100

    WIP: an unused but tested memoization method

---
+++ Yat.Tests/MemoizationTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using System.Net.NetworkInformation;
+using FluentAssertions;
+
+namespace Yat.Tests
+{
+    [TestFixture()]
+    public class MemoizationTest
+    {
+        Memoizator _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new Memoizator();
+        }
+
+        [Test()]
+        public void ShouldCallAMethodSeveralTimeIfArgumentsAreDifferent()
+        {
+            var func = Substitute.For<Func<string, string>>();
+
+            var memoized = _sut.Memoize(func);
+
+            memoized("a");
+            memoized("b");
+            memoized("c");
+
+            func.Received(3);
+        }
+
+        [Test()]
+        public void ShouldAvoidCallingAMethodMoreThanOnceWithTheSameArguments()
+        {
+            var func = Substitute.For<Func<string, string>>();
+            func("a").Returns("foo");
+            var memoized = _sut.Memoize(func);
+
+            memoized("a");
+            memoized("a");
+            memoized("a");
+
+            func.Received(1);
+        }
+
+
+        [Test()]
+        public void ShouldMemoizeMethodCallsWithTheSameArguments()
+        {
+            var func = Substitute.For<Func<string, string>>();
+            func("a").Returns("foo");
+            var memoized = _sut.Memoize(func);
+
+            memoized("a");
+            var result = memoized("a");
+
+            result.Should().Be("foo");
+        }
+    }
+}
+
---
+++ Yat/Memoizator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yat
+{
+    public class Memoizator
+    {
+        public Func<TSource, TReturn> Memoize<TSource, TReturn>(Func<TSource, TReturn> func)
+        {
+            var cache = new Dictionary<TSource, TReturn>();
+            return s =>
+            {
+                if (!cache.ContainsKey(s))
+                {
+                    cache[s] = func(s);
+                }
+                return cache[s];
+            };
+        }
+    }
+}
+
