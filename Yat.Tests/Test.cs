9652bb4bfb95a551280d9be824e4babf05c7b11c
Arialdo Martini <arialdomartini@gmail.com>
2014-12-18 08:31:42 +0100

    A test project, with a dummy test

---
+++ Yat.Tests/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Yat.Tests
+{
+    [TestFixture ()]
+    public class Test
+    {
+        [Test ()]
+        public void ShouldPass ()
+        {
+            Assert.Pass ();
+        }
+    }
+}
+
