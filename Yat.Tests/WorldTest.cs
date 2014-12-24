ada6ce46f5447cd0cc5122dc54f574a00fe5bf94
Arialdo Martini <arialdomartini@gmail.com>
2014-12-24 09:17:52 +0100

    The World can generate random Walks

---
+++ Yat.Tests/WorldTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using FluentAssertions;
+
+namespace Yat.Tests
+{
+    [TestFixture]
+    public class WorldTest
+    {
+        RandomNumberGenerator _randomNumberGenerator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _randomNumberGenerator = new RandomNumberGenerator();
+        }
+
+        [Test]
+        public void ShouldGenerateARandomWalk()
+        {
+            var towns = new List<Town>();
+            for(var i=0; i< 100; i++)
+            {
+                towns.Add(new Town(i, i));
+            }
+            var walk = new Walk(_randomNumberGenerator, towns);
+            var sut = new World(_randomNumberGenerator);
+
+            var actual = sut.GenerateRandomWalk(towns);
+
+            actual.IsCompatibleWith(walk).Should().BeTrue();
+            actual.IsACloneOf(walk).Should().BeFalse();
+
+        }
+    }
+}
+
---
+++ Yat/World.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yat
+{
+    public class World
+    {
+        RandomNumberGenerator _randomNumberGenerator;
+
+        public World(RandomNumberGenerator randomNumberGenerator)
+        {
+            _randomNumberGenerator = randomNumberGenerator;
+        }
+
+        public Walk GenerateRandomWalk(List<Town> towns)
+        {
+            return new Walk(_randomNumberGenerator, towns.OrderBy(i => Guid.NewGuid()).ToList());
+        }
+    }
+}
+
+
